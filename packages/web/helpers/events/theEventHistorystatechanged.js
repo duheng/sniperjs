f5e45d1f6e82418f43d3177a0e6e3949cfef5217
汲国兴 <connorforg@gmail.com>
2020-05-28 11:52:37 +0800

    feat: up historystatechange

--- packages/web/helpers/events/theEventHistorystatechanged.js
+++ packages/web/helpers/events/theEventHistorystatechanged.js
@@ -1,5 +1,12 @@
 import { getElmPath, getCommonAttribute, parseReportLog } from '../utils';
+import Core from '@sniperjs/core/src';
 
-export default (e) => {
-    console.log('theEventHistorystatechanged', e);
+export default (event) => {
+    const { newURL, oldURL } = event;
+
+    const hashchangeLog = parseReportLog(['web_to', newURL, oldURL]);
+    console.log();
+    Core.add({
+        content: hashchangeLog
+    })
 };
