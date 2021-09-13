64a0593e7116a38395c35cf589b7b845291c8fc5
heng.du <heng.du@qunar.com>
2021-09-13 14:19:26 +0800

    update

--- packages/MiniWebErrorReporter/src/index.js
+++ packages/MiniWebErrorReporter/src/index.js
@@ -1,27 +1,27 @@
 import ErrorReporter from '@sniperjs/error-reporter';
-// import pluginHookWeb from './helpers/pluginHookWeb';
-// import pluginHookApp from './helpers/pluginHookApp';
-// import pluginHookRq from './helpers/pluginHookRq';
-// import pluginPatchPromise from './helpers/pluginPatchPromise';
-// import reqeust from './helpers/request';
+import pluginHookWeb from './helpers/pluginHookWeb';
+import pluginHookApp from './helpers/pluginHookApp';
+import pluginHookRq from './helpers/pluginHookRq';
+import pluginPatchPromise from './helpers/pluginPatchPromise';
+import reqeust from './helpers/request';
 class Reportor extends ErrorReporter {
   
   constructor(opts = {}) {
      super(opts);
       // 合并参数
-    //  this.mergeConfig(opts);
-    //  this.init();
+     this.mergeConfig(opts);
+     this.init();
   }
 
   init() {
     // 劫持 App onError
-   // this.use(pluginHookWeb);
+    this.use(pluginHookWeb);
     // // 劫持 Request
     // this.use(pluginHookRq);
    
     // this.use(pluginPatchPromise);
 
-  //  this.applyRequest(reqeust);
+    this.applyRequest(reqeust);
     
   }
 }
